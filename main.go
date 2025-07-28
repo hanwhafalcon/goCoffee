7830de22acf15b28009c822d1636018268141009
Mitchell Geeves <mitchell@geeves-systems.com>
2025-07-28 21:10:03 +1000

    added some notes

--- main.go
+++ main.go
@@ -10,3 +10,18 @@ func main() {
 
 // This is a simple Go program that prints "Hello, World!" to the console.
 // It serves as a basic example of a Go application structure.
+
+// Video tutorial: https://www.youtube.com/watch?v=EfzSmeMVHUw
+
+//CRUD
+// Create, Read, Update, Delete operations are fundamental in many applications.
+// READ - Get all
+// READ - Get specific
+// CREATE - Add new
+// UPDATE - Modify existing
+// DELETE - Remove
+
+// main.go - Contains endpoints and runs server
+// db.go - Contains our db init and connect to database
+// handlers.go - Contains CRUD operations
+// tasks.go - Contains task model and json mappings
