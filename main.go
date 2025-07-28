af0b17b46bdee36632a53368ff6ffe7597ea769c
Mitchell Geeves <mitchell@geeves-systems.com>
2025-07-28 20:04:01 +1000

    example2

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// This is a simple Go program that prints "Hello, World!" to the console.
+// It serves as a basic example of a Go application structure.
